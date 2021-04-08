9ba7235b49b4c7cf83d65ad085362550774a4039
COHL7500 <cohl7500@gmail.com>
2021-04-08 13:27:32 +0200

    update server.py

--- Test/server.py
+++ Test/server.py
@@ -1,18 +1,17 @@
 import socket
 
-host = "147.78.30.122"
-port = 25565
-buff = 1024
+HOST = "147.78.30.122"
+PORT = 25565
 
 succ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-succ.bind((host, port))
+succ.bind((HOST, PORT))
 succ.listen()
 conn, addr = succ.accept()
 
 print("Forbindelse fra: ", str(addr))
 
 while True:
-    (data, addr) = succ.recv(buff)
+    (data, addr) = succ.recv(1024)
 
     print("Received message:" + data)
 
