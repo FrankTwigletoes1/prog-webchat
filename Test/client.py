9ad69569d48b15c8d77dcec9186a76e6235ac07b
COHL7500 <cohl7500@gmail.com>
2021-04-14 16:21:24 +0200

    kommandoer, forbindelse (lokal), chat

--- Test/client.py
+++ Test/client.py
@@ -1,19 +1,50 @@
-import socket
+import socket, threading, time
 
-HOST = '127.0.0.1'
-PORT = 12345
+HOST = '0.0.0.0' #'176.23.70.52'
+PORT = 27015
+SERVER_ALIVE = False
 
 SOCK = socket.socket()
 SOCK.connect((HOST, PORT))
+SOCK.sendto(("!brugernavn " + input("Vælg brugernavn: ")).encode(), (HOST, PORT))
+
+class server:
+    def __init__(self):
+        self.toggle_state()
+        self.thread = threading.Thread(target=self.server_aflytter)
+        self.thread.start()
+
+    def __del__(self):
+        self.thread.should_abort_immediately = True
+
+    def server_aflytter(self):
+        while True:
+            try:
+                data = SOCK.recv(1024)
+                
+            except:
+                print("Serveren afsluttede forbindelsen!")
+                self.toggle_state()
+                break;
+
+            print(data.decode())
+
+        del self
+
+    def toggle_state(self):
+        global SERVER_ALIVE
+        SERVER_ALIVE = not SERVER_ALIVE
+        
+server()
+time.sleep(1)
 
 while True:
-    print(SOCK.recv(1024))
+    data = input(">> ")
     
-    data = input("Enter message to send or type 'exit': ")
-
-    if data == "exit":
+    if data == "!afslut" or not SERVER_ALIVE:
         break
-    else:
+    
+    elif(SERVER_ALIVE):
         SOCK.sendto(data.encode(), (HOST, PORT))
 
 SOCK.close()
--- Test/server.py
+++ Test/server.py
@@ -1,21 +1,78 @@
-import socket
+import socket, threading
 
 HOST = ''
-PORT = 12345
+PORT = 27015
+KLIENTER = []
 
 SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SOCK.bind((HOST, PORT))
 SOCK.listen(5)
 
-print("Socket aflytter...")
+print("Server startet!")
+print("Venter på klienter...")
+
+class klient:
+    def __init__(self, con, addr):
+        self.con = con
+        self.addr = addr
+        self.brugernavn = str(self.addr)
+        
+        print("Forbindelse oprettet fra ", self.addr)
+        
+        self.con.sendto(("Server forbindelse etableret!\nSkriv '!hjælp' for hjælp\n" + str(len(KLIENTER)) + " person(er) herinde lige nu\n").encode(), (HOST, PORT))
+        self.broadcast("En klient oprettede forbindelse.")
+        
+        self.thread = threading.Thread(target=self.klient_aflytter)
+        self.thread.start()
+
+    def __del__(self):
+        self.thread.should_abort_immediately = True
+    
+    def klient_aflytter(self):
+        while True:
+            try:
+                data = self.con.recv(1024)
+                
+            except:
+                print("Klienten ", self.brugernavn ," afsluttede forbindelsen")
+                self.broadcast("\nKlienten " + self.brugernavn + " afsluttede forbindelsen")
+                break
+
+            if not self.klient_kommandoer(data):
+                print(self.brugernavn, self.addr, " >> ", data.decode())
+                self.broadcast("\n" + self.brugernavn + " >> " + data.decode())
+
+        KLIENTER.remove(self)
+        del self
+    
+    def klient_kommandoer(self, besked):
+        if besked.decode() == "!hjælp":
+            self.con.sendto("!hjælp:        Udskriver hjælpemeddelser.\n!brugerliste:  Udskriver en liste over alle brugere.\n!afslut:       Lukker chatprogrammet ned.\n!brugernavn X: Ændre dit brugernavn i chatten.\n".encode(), (HOST, PORT))
+            return True
+            
+        if besked.decode() == "!brugerliste":
+            liste_brugere = "Brugere (" + str(len(KLIENTER)) + "):\n"
+            
+            for index, klnt in enumerate(KLIENTER):
+                liste_brugere += str(index) + ": " + klnt.brugernavn + "\n"
+    
+            self.con.sendto(liste_brugere.encode(), (HOST, PORT))
+            return True
+
+        if besked.decode().split(' ', 1)[0] == "!brugernavn":
+            self.brugernavn = besked.decode().split(' ', 1)[1]
+
+            return True
+    
+        return False
+    
+    def broadcast(self, besked):
+        for klnt in KLIENTER:
+            if klnt is not self:
+                klnt.con.sendto(besked.encode(), (HOST, PORT))
 
 while True:
     CON, ADDR = SOCK.accept()
-
-    print("Forbindelse oprettet fra ", ADDR)
-
-    CON.sendto("Tak for at oprette forbindelse!".encode(), (HOST, PORT))
-
-    print("Modtog besked ", CON.recv(1024), " fra ", ADDR)
+    KLIENTER.append(klient(CON, ADDR))
 
 SOCK.close()
